1543039b280f4129d5cf58c210f6e586945f7fbd
Joseph Moore <josephmoore.it@gmail.com>
2025-05-19 22:23:24 -0400

    add .gitignore and pygame init

---
+++ main.py
@@ -0,0 +1,21 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+def main():
+  pygame.init()
+  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  while True:
+     for event in pygame.event.get():
+       if event.type == pygame.QUIT:
+         return
+     screen.fill((0, 0, 0))  # Fill the screen with black
+     pygame.display.flip()  # Update the display
+  print("Starting Asteroids!")
+  print(f"Screen width: {SCREEN_WIDTH}")
+  print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
